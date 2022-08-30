01cf6bc6ed7ea641eb323577ce8d234806f812a2
Jeppi <jesper.fuella2@gmail.com>
2022-08-30 16:00:44 +0200

    adding files

---
+++ main.py
@@ -0,0 +1,17 @@
+import qrcode
+
+
+
+data = input('Data saved in QrCode: ')
+
+qr = qrcode.QRCode(version = 1, box_size=10, border=5)
+
+qr.add_data(data)
+
+qr.make(fit=True)
+img = qr.make_image(fill_color = 'black', back_color = 'white')
+
+path = 'dist'
+file_name = 'QrCode'
+
+img.save(path + "/" + file_name + ".png")
